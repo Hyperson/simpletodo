d3dbf82010f73204acf8013327e0eb969404a328
Marwen <aoun.marwen@hotmail.com>
2025-10-02 18:30:36 +0200

    add datastore

---
+++ core/domain/src/main/kotlin/com/wasaap/androidstarterkit/core/domain/DeleteTodoUseCase.kt
@@ -0,0 +1,13 @@
+package com.wasaap.androidstarterkit.core.domain
+
+import com.wasaap.androidstarterkit.core.domain.repository.TodoRepository
+import kotlinx.coroutines.flow.Flow
+import com.wasaap.androidstarterkit.core.domain.common.Result
+
+import javax.inject.Inject
+
+class DeleteTodoUseCase @Inject constructor(
+    private val repository: TodoRepository,
+) {
+    operator fun invoke(id: String) : Flow<Result<Unit>> = repository.deleteTodo(id)
+}
